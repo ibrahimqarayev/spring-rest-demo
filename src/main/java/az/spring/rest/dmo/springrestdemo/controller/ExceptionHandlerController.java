ae1dda010c6bf53d81654705c67e67ce3628a10c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-19 10:54:57 +0400

    ExceptionHandlerController

---
+++ src/main/java/az/spring/rest/dmo/springrestdemo/controller/ExceptionHandlerController.java
@@ -0,0 +1,57 @@
+package az.spring.rest.dmo.springrestdemo.controller;
+
+import az.spring.rest.dmo.springrestdemo.enums.ErrorCodeEnum;
+import az.spring.rest.dmo.springrestdemo.exception.CustomNotFoundException;
+import az.spring.rest.dmo.springrestdemo.rest.model.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(CustomNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleCustomException(CustomNotFoundException e) {
+        return ErrorResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInputParam(MethodArgumentTypeMismatchException exception) {
+        String paramName = exception.getParameter().getParameterName();
+
+        return ErrorResponse.builder()
+                .code(ErrorCodeEnum.VALIDATION_ERROR.getCode())
+                .message(paramName + ErrorCodeEnum.VALIDATION_ERROR.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidation(MethodArgumentNotValidException exception) {
+
+        String fieldName = exception.getBindingResult().getFieldError().getField();
+
+        return ErrorResponse.builder()
+                .code(ErrorCodeEnum.VALIDATION_ERROR.getCode())
+                .message(fieldName + ErrorCodeEnum.VALIDATION_ERROR.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnknown(Exception e) {
+        return ErrorResponse.builder()
+                .code(ErrorCodeEnum.UNKNOWN_ERROR.getCode())
+                .message(ErrorCodeEnum.UNKNOWN_ERROR.getMessage())
+                .build();
+    }
+
+}
