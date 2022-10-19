2090e5b3922a1f8bb9095562c646ccb1efca4d07
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-19 10:52:29 +0400

    EmployeeService

---
+++ src/main/java/az/spring/rest/dmo/springrestdemo/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package az.spring.rest.dmo.springrestdemo.service;
+
+import az.spring.rest.dmo.springrestdemo.rest.model.dto.EmployeeDto;
+import az.spring.rest.dmo.springrestdemo.rest.model.response.EmployeeResponse;
+
+
+public interface EmployeeService {
+    EmployeeResponse getAllEmployees();
+
+    EmployeeDto getEmployee(long id);
+
+    EmployeeResponse getEmployeeByNameAndSurname(String name, String surname);
+
+    void insert(EmployeeDto employeeDto);
+
+    void update(EmployeeDto employeeDto, long id);
+
+    void updateSome(EmployeeDto employeeDto, long id);
+
+    void delete(long id);
+}
