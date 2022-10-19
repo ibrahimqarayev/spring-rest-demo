a08bde18fd9da460b56fbf9e33c898d42a2df0fd
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-19 10:48:39 +0400

    EmployeeController

---
+++ src/main/java/az/spring/rest/dmo/springrestdemo/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package az.spring.rest.dmo.springrestdemo.controller;
+
+import az.spring.rest.dmo.springrestdemo.rest.model.dto.EmployeeDto;
+import az.spring.rest.dmo.springrestdemo.rest.model.response.EmployeeResponse;
+import az.spring.rest.dmo.springrestdemo.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+
+    @GetMapping
+    public EmployeeResponse getAllEmployee() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeDto getEmployee(@PathVariable("id") long employeeId) {
+        return employeeService.getEmployee(employeeId);
+    }
+
+    @GetMapping("/search")
+    public EmployeeResponse getEmployeeByNameAndSurname(
+            @RequestParam("name") String name,
+            @RequestParam("surname") String surname) {
+        return employeeService.getEmployeeByNameAndSurname(name, surname);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void insert(@RequestBody @Valid EmployeeDto employeeDto) {
+        employeeService.insert(employeeDto);
+    }
+
+    @PutMapping("/{id}")
+    public void update(
+            @RequestBody  EmployeeDto employeeDto,
+            @PathVariable("id") long id) {
+        employeeService.update(employeeDto, id);
+    }
+
+    @PatchMapping("/{id}")
+    public void updateSome(
+            @RequestBody EmployeeDto employeeDto,
+            @PathVariable("id") long id) {
+        employeeService.updateSome(employeeDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") long id) {
+        employeeService.delete(id);
+
+    }
+
+}
