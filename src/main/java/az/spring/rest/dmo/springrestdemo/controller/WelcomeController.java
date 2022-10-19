7c6bddabaf42a8dc56dcf2213b9af4ed360efd71
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-19 10:54:16 +0400

    WelcomeController

---
+++ src/main/java/az/spring/rest/dmo/springrestdemo/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package az.spring.rest.dmo.springrestdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome Spring Security";
+    }
+}
